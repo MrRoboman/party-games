eed753918f8af2f3fe698815dbce3cdc966cfab2
Rob Kayson <rkayson@gmail.com>
2022-05-18 06:57:16 -0700

    Box falls on ground

--- public/js/game.js
+++ public/js/game.js
@@ -1,4 +1,41 @@
+let world, engine
+let body
+let ground
+
 function setup() {
   createCanvas(800, 600)
   background(120)
+  rectMode(CENTER)
+
+  engine = Matter.Engine.create()
+  world = engine.world
+
+  body = Matter.Bodies.rectangle(100, 100, 100, 100)
+  Matter.World.add(world, body)
+
+  ground = Matter.Bodies.rectangle(width / 2, height - 25, width, 50)
+  Matter.World.add(world, ground)
+  ground.isStatic = true
+}
+
+function draw() {
+  Matter.Engine.update(engine)
+
+  background(120)
+
+  let pos = body.position
+  let angle = body.angle
+  push()
+  translate(pos.x, pos.y)
+  rotate(angle)
+  rect(0, 0, 100, 100)
+  pop()
+
+  pos = ground.position
+  angle = ground.angle
+  push()
+  translate(pos.x, pos.y)
+  rotate(angle)
+  rect(0, 0, width, 50)
+  pop()
 }
